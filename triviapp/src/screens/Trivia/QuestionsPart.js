91be2f7ccd0dcd11ec6766b65ab1484bda4e1001
CoffeePower100 <64426956+CoffeePower100@users.noreply.github.com>
2023-01-01 21:22:40 +0200

    Update QuestionsPart.js

--- triviapp/src/screens/Trivia/QuestionsPart.js
+++ triviapp/src/screens/Trivia/QuestionsPart.js
@@ -1,19 +1,33 @@
-import React, {useEffect,useCallback, useState} from 'react'
+import React, {useState} from 'react'
 import { View, Text, TouchableOpacity, FlatList} from 'react-native';
 import Style from '../../utilis/AppStyle';
 
 
-// fetch questions from api:
 
-/*
-const prepare_questions =  () => {
+
+const QuestionsPart = ({navigation}) => {
+
+  const [questionsArr, setQuestionsArr] = useState([])
+  const [answersArr, setAnswersArr] = useState([]);
+
+  // fetch questions from api:
+
+const prepareQuestions = async() => {
   let i, j = 0;
   let currAnswersArr = [];
-      questionsData => {
-      console.log(JSON.stringify(questionsData));
-      console.log("getched");
-      const questionsRes = questionsData.results;
-      const createdQuestionsArr = [];
+  /*{id: 1, questionText: "QUESTION number 1"}*/
+  const url = "https://opentdb.com/api.php?amount=20&category=18";
+  const response = await fetch(url, {
+      method: 'get',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  const questionsData = await response.json();
+    console.log(JSON.stringify(questionsData));
+    console.log(questionsData.results);
+    const questionsRes = questionsData.results; 
+    const createdQuestionsArr = [];
       for (i = 0; i < questionsRes.length; i++)
       {
           // create answers array:
@@ -25,7 +39,7 @@ const prepare_questions =  () => {
 
           // add correct answer: 
           // (TODO - random index for correct answer)
-          currAnswersArr.push({id: j, answerText: questionsRes[i].correctAnswers});
+          currAnswersArr.push({id: j, answerText: questionsRes[i].correct_answer});
           
 
 
@@ -37,31 +51,12 @@ const prepare_questions =  () => {
       console.log(createdQuestionsArr[0]);
       setQuestionsArr(createdQuestionsArr);
       setAnswersArr((questionsArr.length) ? (questionsArr[0].answersArr) : ([{id: 0, answerText: "No questions found"}]));
-    }
-  }*/
-
-const QuestionsPart = ({navigation}) => {
-  const [questionsArr, setQuestionsArr] = useState([])
-  const [answersArr, setAnswersArr] = useState([]);
-  /*{id: 1, questionText: "QUESTION number 1"}*/
-  
-  const fetchQuestions = async() => {
-    const url = "https://opentdb.com/api.php?amount=20&category=18";
-    const response = await fetch(url, {
-        method: 'get',
-        headers: {
-          "Content-Type": "application/json"
-        }
-    })
-    
-  setQuestionsArr(await response.json());
   }
 
-  fetchQuestions();
+  prepareQuestions();
 
   return(
         <View style={Style.container}>
-            <Text>{questionsArr}</Text>
             <FlatList
               data={answersArr}
               keyExtractor={item => item.id}
