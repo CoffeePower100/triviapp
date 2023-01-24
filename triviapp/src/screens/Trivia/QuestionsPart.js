48ca3d86969ce6168d3872b1a5919d88dff86e12
CoffeePower100 <64426956+CoffeePower100@users.noreply.github.com>
2023-01-24 11:38:10 +0200

    updated trivia questions part

--- triviapp/src/screens/Trivia/QuestionsPart.js
+++ triviapp/src/screens/Trivia/QuestionsPart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useCallback, useState} from 'react'
 import { View, Text, TouchableOpacity, FlatList} from 'react-native';
 import Style from '../../utilis/AppStyle';
 
@@ -9,22 +9,25 @@ const QuestionsPart = ({navigation}) => {
 
   const [questionsArr, setQuestionsArr] = useState([])
   const [answersArr, setAnswersArr] = useState([]);
+  const [isTriviaLoading, setIsTriviaLoading] = useState(true);
 
-  // fetch questions from api:
 
-const prepareQuestions = async() => {
+  // fetch questions and prepare api:
+
+  const prepareQuestions = useCallback(() => {
   let i, j = 0;
   let currAnswersArr = [];
   /*{id: 1, questionText: "QUESTION number 1"}*/
   const url = "https://opentdb.com/api.php?amount=20&category=18";
-  const response = await fetch(url, {
+  fetch(url, {
       method: 'get',
       headers: {
         "Content-Type": "application/json"
       }
     })
-  const questionsData = await response.json();
-    console.log(JSON.stringify(questionsData));
+  .then(response => {
+    response.json()
+    .then(questionsData => {
     console.log(questionsData.results);
     const questionsRes = questionsData.results; 
     const createdQuestionsArr = [];
@@ -47,24 +50,35 @@ const prepareQuestions = async() => {
           // answers array:
           createdQuestionsArr.push({questionText: questionsRes[i].question, answersArr: currAnswersArr});
       }
-      console.log(createdQuestionsArr);
-      console.log(createdQuestionsArr[0]);
       setQuestionsArr(createdQuestionsArr);
       setAnswersArr((questionsArr.length) ? (questionsArr[0].answersArr) : ([{id: 0, answerText: "No questions found"}]));
-  }
+      setIsTriviaLoading(false);
+    })
+  })
+}, []);
+
+  
+  useEffect(() => { 
+  }, [prepareQuestions])
 
   prepareQuestions();
-
+  
   return(
-        <View style={Style.container}>
+    isTriviaLoading ? 
+    (<View> 
+      <Text>Loading...</Text>
+    </View>) :
+    (<View style={Style.container}>
             <FlatList
               data={answersArr}
               keyExtractor={item => item.id}
-              renderItem={rowItem => <Text>{rowItem.item.answerText}</Text>}
+              renderItem={rowItem => 
+              <TouchableOpacity style = {Style.answer_button}>
+                <Text>{rowItem.item.answerText}</Text>
+              </TouchableOpacity>}
             />
-        </View>
-    )
-}
+        </View>)
+  )}
 
 
 export default QuestionsPart;
--- triviapp/src/utilis/AppStyle.js
+++ triviapp/src/utilis/AppStyle.js
@@ -3,20 +3,27 @@ import Colors from './AppColors';
 
 export default StyleSheet.create({
     greeting_container: {
-        flex:1, backgroundColor:Colors.bg
+        flex: 1, 
+        backgroundColor: Colors.bg
     },
     greeting_background: {
         flex: 1
     },
     greeting_button: {
-        alignItems:'center',
-        paddingVertical:15,
-        borderRadius:12,
+        alignItems: 'center',
+        paddingVertical: 15,
+        borderRadius: 12,
         backgroundColor: Colors.yellow,
-        marginTop:20
+        marginTop: 20
     },
     container: {
-        flex:1, backgroundColor:Colors.bg,
-        padding:30
+        flex: 1, 
+        backgroundColor: Colors.bg,
+        padding: 30
+    },
+    answer_button: {
+        flex: 1,
+        backgroundColor: Colors.ocean,
+        fontSize: 20
     }
 })
